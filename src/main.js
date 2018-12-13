fc508bc667e61218f7228cddd46b555a14391570
Marcel Tanner <marcel.tanner@uzh.ch>
2018-12-13 15:03:59 +0100

    changing to local db

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ import c3s from 'vuex-c3s'
 import Croppa from 'vue-croppa';
 
 //let swaggerURL = 'https://api.citizenscience.ch/api/v2/swagger.json';
-let swaggerURL = 'http://172.23.104.29:8080/api/v2/swagger.json';
+let swaggerURL = 'http://localhost:8080/api/v2/swagger.json';
 
 Vue.config.productionTip = false
 
