7570fa95adff7abb148d05a7fce9355729437ac0
Marcel Tanner <marcel.tanner@uzh.ch>
2019-10-02 18:41:38 +0200

    store with localStorage check

--- src/store/store.js
+++ src/store/store.js
@@ -2,8 +2,9 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import settings from './modules/settings'
 import consts from './modules/consts'
-import stats from './modules/stats'
 import gdpr from './modules/gdpr'
+import stats from './modules/stats'
+
 import createPersistedState from 'vuex-persistedstate'
 
 
@@ -11,13 +12,30 @@ Vue.use(Vuex);
 
 const debug = process.env.NODE_ENV !== 'production';
 
-export default new Vuex.Store({
-  modules: {
-    settings,
-    consts,
-      stats,
-      gdpr
-  },
-  strict: debug,
-  plugins: [createPersistedState({ storage: window.localStorage})]
-});
+
+let store;
+if( window.localStorage ) {
+  store = new Vuex.Store({
+    modules: {
+      settings,
+      consts,
+      gdpr,
+      stats
+    },
+    strict: debug,
+    plugins: [createPersistedState({ storage: window.localStorage})]
+  });
+}
+else {
+  store = new Vuex.Store({
+    modules: {
+      settings,
+      consts,
+      gdpr,
+      stats
+    },
+    strict: debug
+  });
+}
+
+export default store;
